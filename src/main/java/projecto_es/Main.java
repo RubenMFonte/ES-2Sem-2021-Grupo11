6b0f50a00a954c34f3ed1c97aedcda6bd4e74a50
RubenMFonte <a919.ruben@gmail.com>
2021-04-01 18:41:49 +0100

    added loc_class method

--- src/main/java/projecto_es/Main.java
+++ src/main/java/projecto_es/Main.java
@@ -1,6 +1,18 @@
 package projecto_es;
 
+import java.io.FileNotFoundException;
+
+import com.github.javaparser.JavaParser;
+
 public class Main {
 	public static void main(String[] args) {
+		
+		try {
+			MetricsCalculator m = MetricsCalculator.getMetricsCalculatorInstance();
+			m.run("MetricsCalculator.java");
+		}
+		catch(FileNotFoundException e){
+			
+		}
 	}
 }
--- src/main/java/projecto_es/MetricsCalculator.java
+++ src/main/java/projecto_es/MetricsCalculator.java
@@ -1,11 +1,22 @@
 package projecto_es;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseException;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.visitor.GenericVisitorAdapter;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 
-public class MetricsCalculator {
+public class MetricsCalculator {	
+	
 	// Singleton instance
 	private static MetricsCalculator metricsCalculator = null;
 	
@@ -17,15 +28,20 @@ public class MetricsCalculator {
 		compilationUnits = new ArrayList<CompilationUnit>();
 	}
 	
-	public MetricsCalculator getMetricsCalculatorInstance()
+	public static MetricsCalculator getMetricsCalculatorInstance()
 	{
 		if(metricsCalculator == null) metricsCalculator = new MetricsCalculator();
 		
 		return metricsCalculator;
 	}
 	
-	public void run(String filename) {
-		// Start of the metrics calculation process
+	public void run(String filename) throws FileNotFoundException {
+		JavaParser parser = new JavaParser();
+		File file = new File("src/main/java/projecto_es/MetricsCalculator.java");
+	}
+	
+	private int getLOC_Class(ClassOrInterfaceDeclaration classNode) {
+		return classNode.toString().split("\r\n").length;
 	}
 
 }
