5074208092971e47830b6782ba9ac30929bc86a9
JoaoFernandes <47919146+JoaoSaFernandes@users.noreply.github.com>
2021-04-05 18:11:02 +0100

    interface (incomplete)

--- src/main/java/projecto_es/InterfaceMetricsStatistics.java
+++ src/main/java/projecto_es/InterfaceMetricsStatistics.java
@@ -12,6 +12,7 @@ import javax.swing.JTextField;
 import java.awt.FlowLayout;
 import javax.swing.JScrollPane;
 import javax.swing.BoxLayout;
+import javax.swing.DefaultListModel;
 import javax.swing.JLabel;
 import java.awt.Color;
 import javax.swing.JButton;
@@ -29,15 +30,49 @@ public class InterfaceMetricsStatistics extends JFrame {
 	private JPanel contentPane;
 	private JTable table;
 	private JTable table_1;
+	
+	private JList packageJList;
+	private JList classJList;
+	private JList methodsJList;
+	private JList metricsJlist;
+	private JList statisticsJlist;
+	
+	private String packageString;
+	private String classString;
+	private String methodsString;
 
 	/**
-	 * Launch the application.
-	 */
+	 * Launch the application. */
+	 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
+				 final DefaultListModel<String> l1 = new DefaultListModel<>();  
+		          l1.addElement("C");  
+		          l1.addElement("C++");  
+		          l1.addElement("Java");  
+		          l1.addElement("PHP");  
+		          final JList<String> packageJList = new JList<>(l1); 
+		          final DefaultListModel<String> l2 = new DefaultListModel<>();  
+		          l2.addElement("C1");  
+		          l2.addElement("C++1");  
+		          l2.addElement("Java1");  
+		          l2.addElement("PHP1");  
+		          final JList<String> classJList = new JList<>(l2); 
+		          final DefaultListModel<String> l3 = new DefaultListModel<>();  
+		          l3.addElement("C2");  
+		          l3.addElement("C++2");  
+		          l3.addElement("Java2");  
+		          l3.addElement("PHP2");  
+		          final JList<String> methodsJList = new JList<>(l3); 
+		          final DefaultListModel<String> l4 = new DefaultListModel<>();  
+		          l4.addElement("C3");  
+		          l4.addElement("C++3");  
+		          l4.addElement("Java3");  
+		          l4.addElement("PHP3");  
+		          final JList<String> statisticsJlist = new JList<>(l4); 
 				try {
-					InterfaceMetricsStatistics frame = new InterfaceMetricsStatistics();
+					InterfaceMetricsStatistics frame = new InterfaceMetricsStatistics(packageJList,classJList,methodsJList,statisticsJlist);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -49,7 +84,12 @@ public class InterfaceMetricsStatistics extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public InterfaceMetricsStatistics() {
+	public InterfaceMetricsStatistics(JList packageJList, JList classJList, JList methodsJList, JList statisticsJlist) {
+		this.packageJList = packageJList;
+		this.classJList = classJList;
+		this.methodsJList = methodsJList;
+		this.statisticsJlist = statisticsJlist;
+		
 		setTitle("Statistics");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 622, 371);
@@ -58,6 +98,7 @@ public class InterfaceMetricsStatistics extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(new GridLayout(1, 5, 0, 0));
 		
+//package JList		
 		JPanel contentPane_Package = new JPanel();
 		contentPane_Package.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		contentPane.add(contentPane_Package);
@@ -71,10 +112,10 @@ public class InterfaceMetricsStatistics extends JFrame {
 		JScrollPane scrollPane_1 = new JScrollPane();
 		contentPane_Package.add(scrollPane_1);
 		
-		JList list_1 = new JList();
-		scrollPane_1.setViewportView(list_1);
+		//JList list_1 = new JList();
+		scrollPane_1.setViewportView(packageJList);
 		
-		
+//class JList		
 		JPanel contentPane_Classes = new JPanel();
 		contentPane_Classes.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		contentPane.add(contentPane_Classes);
@@ -88,9 +129,10 @@ public class InterfaceMetricsStatistics extends JFrame {
 		JScrollPane scrollPane_2 = new JScrollPane();
 		contentPane_Classes.add(scrollPane_2);
 		
-		JList list_2 = new JList();
-		scrollPane_2.setViewportView(list_2);
-		
+		//JList list_2 = new JList();
+		scrollPane_2.setViewportView(classJList);
+
+//methods JList
 		JPanel contentPane_Methods = new JPanel();
 		contentPane_Methods.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		contentPane.add(contentPane_Methods);
@@ -104,9 +146,9 @@ public class InterfaceMetricsStatistics extends JFrame {
 		JScrollPane scrollPane_3 = new JScrollPane();
 		contentPane_Methods.add(scrollPane_3);
 		
-		JList list_3 = new JList();
-		scrollPane_3.setViewportView(list_3);
-		
+		//JList list_3 = new JList();
+		scrollPane_3.setViewportView(methodsJList);
+//metrics JList		
 		JPanel contentPane_Metrics = new JPanel();
 		contentPane.add(contentPane_Metrics);
 		contentPane_Metrics.setLayout(new BoxLayout(contentPane_Metrics, BoxLayout.Y_AXIS));
@@ -119,9 +161,13 @@ public class InterfaceMetricsStatistics extends JFrame {
 		JScrollPane scrollPane = new JScrollPane();
 		contentPane_Metrics.add(scrollPane);
 		
-		table = new JTable();
-		scrollPane.setViewportView(table);
+		//JList metricsJlist = new JList();
+		scrollPane.setViewportView(metricsJlist);
 		
+		JButton buttonShow = new JButton("Show");
+		contentPane_Metrics.add(buttonShow);
+		
+//statistics JList		
 		JPanel panel_1 = new JPanel();
 		contentPane.add(panel_1);
 		panel_1.setLayout(new BoxLayout(panel_1, BoxLayout.Y_AXIS));
@@ -134,8 +180,23 @@ public class InterfaceMetricsStatistics extends JFrame {
 		JScrollPane scrollPane_4 = new JScrollPane();
 		panel_1.add(scrollPane_4);
 		
-		table_1 = new JTable();
-		scrollPane_4.setViewportView(table_1);
+		scrollPane_4.setViewportView(statisticsJlist);
+		
+		JButton goBackButton = new JButton("Go Back");
+		panel_1.add(goBackButton);
+/*		
+//button actionListener
+		buttonShow.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 if (packageJList.getSelectedIndex() != -1 && classJList.getSelectedIndex() != -1 &&  methodsJList.getSelectedIndex() != -1) {                       
+	                    packageString = (String) packageJList.getSelectedValue(); 
+	                    classString = (String) classJList.getSelectedValue(); 
+	                    methodsString = (String) methodsJList.getSelectedValue(); 
+	                 } 
+				 metricsJlist.setListData(COLOCAR a FUNÇÂO que devolve a lista de METRICAS);
+			}
+		});  */
 	}
+
 
 }
