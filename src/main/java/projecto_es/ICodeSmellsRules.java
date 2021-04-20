95bcaa247b8734679c375a2e6443f9e01c9c1e64
chico88687 <fvieira9835@gmail.com>
2021-04-20 14:19:40 +0100

    Classe teste para JTable e Testes na ICodeSmellsRule

--- src/main/java/projecto_es/ICodeSmellsRules.java
+++ src/main/java/projecto_es/ICodeSmellsRules.java
@@ -12,6 +12,8 @@ import java.awt.Insets;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Panel;
+import java.awt.Point;
+
 import javax.swing.JButton;
 import javax.swing.JTable;
 import java.awt.BorderLayout;
@@ -23,10 +25,18 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.awt.event.ActionEvent;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.MatteBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+
 import java.awt.Color;
 import javax.swing.border.LineBorder;
 import javax.swing.JScrollPane;
@@ -43,14 +53,15 @@ public class ICodeSmellsRules {
 	private JButton editRule;
 	private JButton export;
 	private JButton activateRule;
+	private JButton filter;
 	private JList codeSmells;
 	private JScrollPane scrollPane;
 	private JScrollPane scrollPane_1;
 	private JLabel labelCodeSmellsDisp;
 	private JLabel ruleHistory;
 	private JLabel activeRule;
-	private ArrayList<Rule> allRules;
-	
+	private List<Rule> allRules;
+	private List<String> allCodeSmells;
 
 	/**
 	 * Launch the application.
@@ -60,7 +71,7 @@ public class ICodeSmellsRules {
 			public void run() {
 				try {
 					ICodeSmellsRules window = new ICodeSmellsRules();
-					
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -70,17 +81,18 @@ public class ICodeSmellsRules {
 
 	/**
 	 * Create the application.
-	 * @throws FileNotFoundException 
+	 * 
+	 * @throws FileNotFoundException
 	 */
 	public ICodeSmellsRules() throws FileNotFoundException {
 		allRules = new ArrayList<>();
 		File saveRule = new File("saveRule.txt");
 		Scanner myReader = new Scanner(saveRule);
-		 while (myReader.hasNextLine()) {
-		   String data = myReader.nextLine();
-		   allRules.add(new Rule(data));
-		 }
-		 myReader.close();
+		while (myReader.hasNextLine()) {
+			String data = myReader.nextLine();
+			allRules.add(new Rule(data));
+		}
+		myReader.close();
 		initialize();
 		frmCodeSmells.setVisible(true);
 	}
@@ -94,66 +106,165 @@ public class ICodeSmellsRules {
 		frmCodeSmells.setTitle("Code Smells & Rules");
 		frmCodeSmells.setBounds(100, 100, 1100, 800);
 		frmCodeSmells.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		newRule = new JButton("Criar Regra");
 		newRule.setFont(new Font("Arial", Font.PLAIN, 12));
-		
+
 		boolean isUniq;
-		ArrayList<String> allCodeSmells = new ArrayList<>();
+		allCodeSmells = new ArrayList<>();
 		allCodeSmells.add("All");
-		for(int i = 0; i<allRules.size();i++) {
+		for (int i = 0; i < allRules.size(); i++) {
 			isUniq = true;
-			for(int j=0; j<allCodeSmells.size();j++) {
-				if(allCodeSmells.get(j).equals(allRules.get(i).getCodeSmell())) {
+			for (int j = 0; j < allCodeSmells.size(); j++) {
+				if (allCodeSmells.get(j).equals(allRules.get(i).getCodeSmell())) {
 					isUniq = false;
 					break;
 				}
 			}
-			if(isUniq) {
+			if (isUniq) {
 				allCodeSmells.add(allRules.get(i).getCodeSmell());
 			}
-			
+
 		}
 		String[] allCodeSmellsJL = new String[allCodeSmells.size()];
-		for(int i = 0;i<allCodeSmells.size();i++) {
-			allCodeSmellsJL[i]=allCodeSmells.get(i);
+		for (int i = 0; i < allCodeSmells.size(); i++) {
+			allCodeSmellsJL[i] = allCodeSmells.get(i);
 		}
 		codeSmells = new JList(allCodeSmellsJL);
 		codeSmells.setBorder(new LineBorder(new Color(0, 0, 0)));
-		
+		codeSmells.setSelectedIndex(0);
+
 		scrollPane = new JScrollPane();
 		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_ALWAYS);
 		scrollPane_1 = new JScrollPane();
-		
+
 		labelCodeSmellsDisp = new JLabel("Code Smells disponiveis");
 		labelCodeSmellsDisp.setFont(new Font("Arial", Font.PLAIN, 17));
-		
+
 		editRule = new JButton("Editar Regra");
 		editRule.setFont(new Font("Arial", Font.PLAIN, 12));
-		
+
 		export = new JButton("Exportar");
 		export.setFont(new Font("Arial", Font.PLAIN, 12));
-		
+
 		activateRule = new JButton("Activar regra");
 		activateRule.setFont(new Font("Arial", Font.PLAIN, 12));
-		
+
 		ruleHistory = new JLabel("Hist\u00F3rico de Regras ");
 		ruleHistory.setFont(new Font("Arial", Font.PLAIN, 17));
-		
+
 		activeRule = new JLabel("Regra Activa:");
 		activeRule.setFont(new Font("Arial", Font.PLAIN, 24));
+		
+		setFilterButton();
+		
+//		draw();
+		
+		createTable(allRules);
+		String[] columnNames = { "Regra", "Condição" };
+		String[][] activeRule = new String[1][2];
+		for (int i = 0; i < allRules.size(); i++) {
+			if (allRules.get(i).isActive())
+				activeRule[0][0] = allRules.get(i).getID();
+			activeRule[0][1] = allRules.get(i).onlyConditions();
+		}
+		activatedRule = new JTable(activeRule, columnNames);
+		activatedRule.setFillsViewportHeight(true);
+		scrollPane_1.setViewportView(activatedRule);
+		scrollPane.setViewportView(rules);
+
+		selectAction();
+	}
+
+//	public List<Integer> filterRule(String codeSmell) {
+//		List<Integer> listFiltered = new ArrayList<>();
+//		for (int i = 0; i < allRules.size(); i++) {
+//			if (allRules.get(i).getCodeSmell().equals(codeSmell))
+//				listFiltered.add(Integer.parseInt(allRules.get(i).getID()));
+//		}
+//		return listFiltered;
+//	}
+	
+	public List<Rule> filterRule(String codeSmell) {
+		List<Rule> listFiltered = new ArrayList<>();
+		for (int i = 0; i < allRules.size(); i++) {
+			if (allRules.get(i).getCodeSmell().equals(codeSmell))
+				listFiltered.add(allRules.get(i));
+		}
+		return listFiltered;
+	}
+
+	public void createTable(List<Rule> rulesList) {
+		String[] columnNames = { "Regra", "Condição" };
+		String[][] allRulesJT = new String[rulesList.size()][2];
+		for (int i = 0; i < rulesList.size(); i++) {
+			allRulesJT[i][0] = rulesList.get(i).getID();
+			allRulesJT[i][1] = rulesList.get(i).onlyConditions();
+		}
+		rules = new JTable(allRulesJT, columnNames);
+		rules.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		rules.setFillsViewportHeight(true);
+		printRows();;
+		frmCodeSmells.add(rules);
+		System.out.println("add");
+		draw();
+//		frmCodeSmells.add(rules);
+	}
+	
+	public void printRows() {
+		for(int i = 0; i < rules.getRowCount(); i++) {
+			System.out.println("linha " + i + " = " + rules.getValueAt(i, 0) + ":" + rules.getValueAt(i, 1));
+		}
+	}
+	public void selectAction() {
+		codeSmells.addListSelectionListener(new ListSelectionListener() {
+
+			@Override
+			public void valueChanged(ListSelectionEvent e) {
+				System.out.println("to aqui");
+				int index = codeSmells.getSelectedIndex();
+				String cs = allCodeSmells.get(index);
+				createTable(filterRule(cs));
+			}
+		});
+		;
+	}
+ 
+	public void setFilterButton() {
+		filter = new JButton("filtrar");
+		
+//		filter.addActionListener(new ActionListener() {
+//			
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				System.out.println("to aqui");
+//				int index = codeSmells.getSelectedIndex();
+//				String cs = allCodeSmells.get(index);
+//				updateTable(filterRule(cs));
+//				System.out.println(filterRule(cs));
+//				
+//			}
+//		});
+	}
+	
+	public void draw() {
 		GroupLayout groupLayout = new GroupLayout(frmCodeSmells.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.TRAILING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(23)
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 						.addGroup(groupLayout.createSequentialGroup()
-							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-								.addComponent(labelCodeSmellsDisp)
-								.addComponent(codeSmells, GroupLayout.PREFERRED_SIZE, 305, GroupLayout.PREFERRED_SIZE))
-							.addGap(18))
-						.addComponent(activeRule, GroupLayout.PREFERRED_SIZE, 155, GroupLayout.PREFERRED_SIZE))
+							.addGap(23)
+							.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+										.addComponent(labelCodeSmellsDisp)
+										.addComponent(codeSmells, GroupLayout.PREFERRED_SIZE, 305, GroupLayout.PREFERRED_SIZE))
+									.addGap(18))
+								.addComponent(activeRule, GroupLayout.PREFERRED_SIZE, 155, GroupLayout.PREFERRED_SIZE)))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(34)
+							.addComponent(filter)))
 					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
 						.addComponent(scrollPane_1, GroupLayout.DEFAULT_SIZE, 621, Short.MAX_VALUE)
 						.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 621, Short.MAX_VALUE))
@@ -180,7 +291,9 @@ public class ICodeSmellsRules {
 							.addComponent(labelCodeSmellsDisp, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)
 							.addPreferredGap(ComponentPlacement.RELATED)
 							.addComponent(codeSmells, GroupLayout.PREFERRED_SIZE, 496, GroupLayout.PREFERRED_SIZE)
-							.addGap(121)
+							.addGap(28)
+							.addComponent(filter)
+							.addGap(72)
 							.addComponent(activeRule)
 							.addGap(10))
 						.addGroup(groupLayout.createSequentialGroup()
@@ -191,7 +304,7 @@ public class ICodeSmellsRules {
 									.addComponent(editRule, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE)
 									.addGap(500)
 									.addComponent(export, GroupLayout.PREFERRED_SIZE, 23, GroupLayout.PREFERRED_SIZE))
-								.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 625, Short.MAX_VALUE))
+								.addComponent(scrollPane, GroupLayout.DEFAULT_SIZE, 628, Short.MAX_VALUE))
 							.addGap(18)
 							.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
 								.addComponent(scrollPane_1, GroupLayout.PREFERRED_SIZE, 48, GroupLayout.PREFERRED_SIZE)
@@ -200,32 +313,8 @@ public class ICodeSmellsRules {
 					.addGap(45))
 		);
 		
-		String[] columnNames = {"Regra", "Condição"};
-		String[][] allRulesJT = new String[allRules.size()][2];
-		for(int i = 0; i<allRules.size();i++) {
-			allRulesJT[i][0] = allRules.get(i).getID();
-			allRulesJT[i][1] = allRules.get(i).onlyConditions();
-		}
-		rules = new JTable(allRulesJT, columnNames);
-		rules.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		rules.setFillsViewportHeight(true);
-		
-		String[][] activeRule= new String[1][2];
-		for(int i = 0; i<allRules.size();i++) {
-			if(allRules.get(i).isActive())
-				activeRule[0][0] = allRules.get(i).getID();
-				activeRule[0][1] = allRules.get(i).onlyConditions();
-		}
-		activatedRule = new JTable(activeRule,columnNames);
-		activatedRule.setFillsViewportHeight(true);
-		scrollPane_1.setViewportView(activatedRule);
-		scrollPane.setViewportView(rules);
-		
-		
 		frmCodeSmells.getContentPane().setLayout(groupLayout);
+		frmCodeSmells.repaint();
+		System.out.println("drawed");
 	}
-	
-	
-	
-	
 }
--- src/main/java/projecto_es/Rule.java
+++ src/main/java/projecto_es/Rule.java
@@ -2,14 +2,14 @@ package projecto_es;
 
 import java.util.ArrayList;
 
-
 public class Rule {
-	
-	//Este array é do formato ArrayList[(id),(code smell),(active),(condition1),(logicalOperator),(condition2),...]
+
+	// Este array é do formato ArrayList[(id),(code
+	// smell),(active),(condition1),(logicalOperator),(condition2),...]
 	private ArrayList<String> list;
-	
-	
-	//Assume-se que a string que entra nesta função +e do formato "id:codeSmell:active:Metrics:NumericOperator:threshold:LogicalOperator:Metrics:NumericOperator:threshold:LogicalOperator:..."
+
+	// Assume-se que a string que entra nesta função +e do formato
+	// "id:codeSmell:active:Metrics:NumericOperator:threshold:LogicalOperator:Metrics:NumericOperator:threshold:LogicalOperator:..."
 	public Rule(String rule) {
 		list = new ArrayList<>();
 		String[] values = rule.split(":");
@@ -18,19 +18,19 @@ public class Rule {
 		list.add(values[2]);
 		LogicalOperator operator;
 		Condition condition;
-		for(int i = 3; i<values.length;i++) {
-			if(i==5) {
-				condition = new Condition(values[i-2]+":"+values[i-1]+":"+values[i]);
+		for (int i = 3; i < values.length; i++) {
+			if (i == 5) {
+				condition = new Condition(values[i - 2] + ":" + values[i - 1] + ":" + values[i]);
 				list.add(condition.toString());
-			}else if((i-1)%4==0) {
-				if(values[i-3].equals("AND")) {
+			} else if ((i - 1) % 4 == 0) {
+				if (values[i - 3].equals("AND")) {
 					operator = LogicalOperator.AND;
-				}else {
+				} else {
 					operator = LogicalOperator.OR;
 				}
-				condition = new Condition(values[i-2]+":"+values[i-1]+":"+values[i]);
+				condition = new Condition(values[i - 2] + ":" + values[i - 1] + ":" + values[i]);
 				addCondition(operator, condition);
-				
+
 			}
 		}
 	}
@@ -39,88 +39,85 @@ public class Rule {
 		list.add(operator.toString());
 		list.add(condition.toString());
 	}
-	
-	
+
 	public String onlyConditions() {
 		String string = "";
-		for(int i = 3; i<list.size();i++) {
-			if(i%2!=0) {
+		for (int i = 3; i < list.size(); i++) {
+			if (i % 2 != 0) {
 				string = string + list.get(i);
-			}else{
-				string = string + ":" + list.get(i)+":";				
+			} else {
+				string = string + ":" + list.get(i) + ":";
 			}
 		}
 		return string;
 	}
-	
+
 	public void changeID(int id) {
-		list.set(0,Integer.toString(id));
+		list.set(0, Integer.toString(id));
 	}
-	
+
 	public String getID() {
 		return list.get(0);
 	}
-	
+
 	public boolean isActive() {
 		return Boolean.parseBoolean(list.get(2));
 	}
+
 	public void activateRule() {
-		
+
 	}
-	
+
 	public ArrayList<String> getList() {
 		return list;
 	}
-	
+
 	public int numberOfConditions() {
-		return (list.size()-1)/2;
+		return (list.size() - 1) / 2;
 	}
-	
+
 	public String getCondition(int index) {
-		if(index<list.size())
-			return list.get(index*2+1);
+		if (index < list.size())
+			return list.get(index * 2 + 1);
 		else {
 			System.out.println("Erro: Index maior do que o tamanho da lista de condições");
 			return null;
 		}
 	}
-	
-	public String getLogicalOperator(int index){
-		if(index<list.size())
-			return list.get(index+1);
+
+	public String getLogicalOperator(int index) {
+		if (index < list.size())
+			return list.get(index + 1);
 		else {
 			System.out.println("Erro: Index maior do que o tamanho da lista de condições");
 			return null;
 		}
 	}
-	
+
 	public String getCodeSmell() {
 		return list.get(1);
 	}
-	
-	
+
 	@Override
 	public String toString() {
 		String string;
-		string = list.get(0) + ":"+list.get(1)+":"+list.get(2)+":";
-		for(int i = 3; i<list.size();i++) {
-			if(i%2!=0) {
+		string = list.get(0) + ":" + list.get(1) + ":" + list.get(2) + ":";
+		for (int i = 3; i < list.size(); i++) {
+			if (i % 2 != 0) {
 				string = string + list.get(i);
-			}else{
-				string = string + ":" + list.get(i)+":";				
+			} else {
+				string = string + ":" + list.get(i) + ":";
 			}
 		}
 		return string;
 	}
-	
-	
-	
-	//teste de exemplo
+
+	// teste de exemplo
 	public static void main(String[] args) {
 		String a = "0:God_Class:false:WMC_CLASS:LT:4";
 		String b = "LOC_METHOD:EQ:6";
 		String c = "CYCLO_METHOD:GT:7";
-		String d = a+":"+"AND"+":"+b+":"+"OR"+":"+c;
+		String d = a + ":" + "AND" + ":" + b + ":" + "OR" + ":" + c;
 		Rule rule = new Rule(d);
 		System.out.println(d);
 		System.out.println(rule.toString());
@@ -128,7 +125,8 @@ public class Rule {
 		System.out.println(rule.getCondition(2));
 		System.out.println(rule.numberOfConditions());
 		rule.changeID(2);
-		System.out.println(rule.getCodeSmell());;
+		System.out.println(rule.getCodeSmell());
+		;
 		System.out.println(rule.toString());
 
 	}
