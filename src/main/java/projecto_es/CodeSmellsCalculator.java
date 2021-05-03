256c4094d9a6ef2af8e7ad276e7a797d8346568d
jmbsk-iscte <jmbsk@iscte-iul.pt>
2021-05-03 14:51:20 +0100

    Função fillCodeSmellTable() da classe CodeSmellCalculator praticamente acabada, alterei o IMenu para resizable(false).

--- src/main/java/projecto_es/CodeSmellsCalculator.java
+++ src/main/java/projecto_es/CodeSmellsCalculator.java
@@ -1,20 +1,25 @@
 package projecto_es;
 
+import java.awt.Font;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
+
 public class CodeSmellsCalculator {
 
 	private List<ClassDataStructure> dataList;
 	private List<Rule> rules;
-	private List<CodeSmellStatistics> statisics;
+	private List<CodeSmellStatistics> statistics;
 	
-	public CodeSmellsCalculator() {
+	public CodeSmellsCalculator(String path) throws FileNotFoundException {
 		dataList = new ArrayList<>();
 		rules = new ArrayList<>();
+		this.run(path);
 	}
 		
 	public void getCodeSmellsActiveRules() throws FileNotFoundException{
@@ -32,8 +37,28 @@ public class CodeSmellsCalculator {
 	}
 		
 	//Podem alterar a assinatura do método se vos for conveniente
-	public void fillCodeSmellTable() {
-			
+	public JTable fillCodeSmellTable() {
+		String[] columnNames = { "Class", "is_God_Class", "Classificação", "Method ID", "Method Name", "is_long_method", "Classificação" };
+		int statisticsJTNumberLines = 0;
+		for(int c = 0; c < dataList.size(); c++) {
+			statisticsJTNumberLines +=dataList.get(c).getMethodDataStructureList().size();
+		}
+		String[][] statisticsJT = new String[statisticsJTNumberLines][columnNames.length];
+		int line = 0;
+		for(int i=0; i<dataList.size(); i++) {
+			ClassDataStructure data = dataList.get(i);
+			for(int j = 0; j<data.getMethodDataStructureList().size(); j++) {
+				statisticsJT[line][0] = data.getClassName();
+				statisticsJT[line][1] = data.getCodeSmellsEvaluation("God_class").toString();
+				statisticsJT[line][2] = ""; //classificação
+				statisticsJT[line][3] = ""; //data.getMethodDataStructureList().get(j).getID(); Falta criar o metodo para ir buscar o ID do excel
+				statisticsJT[line][4] = data.getMethodDataStructureList().get(j).getMethodName();
+				statisticsJT[line][5] = data.getMethodDataStructureList().get(j).getCodeSmellsEvaluation("Long_method").toString();
+				statisticsJT[line][6] = ""; //Classificação
+				line++;
+			}
+		}
+		return new JTable(statisticsJT, columnNames);
 	}
 		
 	//Podem alterar a assinatura do método se vos for conveniente
@@ -49,23 +74,24 @@ public class CodeSmellsCalculator {
 		return rules;
 	}
 	
-	public void run(String filename) {
-		
+	public void run(String filename) throws FileNotFoundException {
+	    this.getCodeSmellsActiveRules();
+	    this.calculateCodeSmellStatistics();
 	}
 
 		
 	public static void main(String[] args) {
 		//EXEMPLO DO USO DA FUNÇÃO getCodeSmellsActiveRules
-		try {
-			CodeSmellsCalculator teste = new CodeSmellsCalculator();
-			teste.getCodeSmellsActiveRules();
-			List<Rule> arrayTeste = teste.getRule();
-			for(Rule rule : arrayTeste) {
-				System.out.println(rule.toString());
-			}
-		} catch (FileNotFoundException e) {
-					e.printStackTrace();
-		}	
+//		try {
+//			CodeSmellsCalculator teste = new CodeSmellsCalculator();
+//			teste.getCodeSmellsActiveRules();
+//			List<Rule> arrayTeste = teste.getRule();
+//			for(Rule rule : arrayTeste) {
+//				System.out.println(rule.toString());
+//			}
+//		} catch (FileNotFoundException e) {
+//					e.printStackTrace();
+//		}	
 	}
 
 }
