0b9dce4835479388b2f9b50e14c67e1b45f4fe65
RubenMFonte <a919.ruben@gmail.com>
2021-05-05 12:32:00 +0100

    add classbooleanobject and methodboolean

---
+++ src/main/java/projecto_es/MethodBoolean.java
@@ -0,0 +1,18 @@
+package projecto_es;
+public class MethodBoolean{
+	private String methodName;
+	private Boolean lmethod;
+	
+	
+	public MethodBoolean (String methodName, Boolean lmethod) {
+		this.methodName = methodName;
+		this.lmethod= lmethod;
+	}
+	public String getMethodName() {
+		return methodName;
+	}
+	public Boolean getLmethod() {
+		return lmethod;
+	}
+	
+}
